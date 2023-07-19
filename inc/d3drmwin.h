9e5f23bfc7b5fe622eb57f9cb8a0763524f5fee2
Joshua Peisach <itzswirlz2020@outlook.com>
2023-07-19 12:22:10 -0400

    Document d3drmwin

--- inc/d3drmwin.h
+++ inc/d3drmwin.h
@@ -18,7 +18,7 @@
 #include "ddraw.h"
 #include "d3d.h"
 
-/*
+/**
  * GUIDS used by Direct3DRM Windows interface
  */
 DEFINE_GUID(IID_IDirect3DRMWinDevice,	0xc5016cc0, 0xd273, 0x11ce, 0xac, 0x48, 0x0, 0x0, 0xc0, 0x38, 0x25, 0xa1);
@@ -37,10 +37,10 @@ DECLARE_INTERFACE_(IDirect3DRMWinDevice, IDirect3DRMObject)
      * IDirect3DRMWinDevice methods
      */
 
-    /* Repaint the window with the last frame which was rendered. */
+    /// Repaint the window with the last frame which was rendered
     STDMETHOD(HandlePaint)(THIS_ HDC hdc) PURE;
 
-    /* Respond to a WM_ACTIVATE message. */
+    /// Respond to a WM_ACTIVATE message.
     STDMETHOD(HandleActivate)(THIS_ WORD wparam) PURE;
 };
 
